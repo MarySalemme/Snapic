87d83f3aa11e106522aa473a2dc81756c745441a
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-10-17 09:43:33 +0100

    Initial Commit

---
+++ snapic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapic
+//
+//  Created by Mary Salemme on 17/10/2019.
+//  Copyright © 2019 Mary Salemme . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
