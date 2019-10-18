dd0dce819c875fcd1f23b661d0a18edd21bd5e27
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-10-18 09:30:52 +0100

    Implement hardcoded filter on image

--- snapic/ViewController.swift
+++ snapic/ViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet var imageView: UIView!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let filePath = Bundle.main.url(forResource: "image", withExtension: "JPG")
+
+        let originalImage = CIImage(contentsOf: filePath!)
+
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(originalImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        guard let newImage = filter?.outputImage else {
+            return
+        }
+        
+        let image = convert(ciImage: newImage)
+
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
     }
-
-
+    
+    func convert(ciImage:CIImage) -> UIImage {
+        let context: CIContext = CIContext.init(options: nil)
+        let cgImage: CGImage = context.createCGImage(ciImage, from: ciImage.extent)!
+        let image: UIImage = UIImage.init(cgImage: cgImage)
+        return image
+    }
 }
+
+
+
 
